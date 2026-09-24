d77ee2147934b34c3b46bfacf606f5774005e444
it24100198 <it24100198@my.sliit.lk>
2026-09-25 00:21:47 +0530

    Delete fare-payment-service directory

--- fare-payment-service/src/main/java/com/ridelink/fare/exception/PaymentFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.ridelink.fare.exception;
-
-public class PaymentFailedException extends RuntimeException {
-    public PaymentFailedException(String message) {
-        super(message);
-    }
-}
